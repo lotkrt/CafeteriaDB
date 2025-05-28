c89935166f8b95e1ac36a3568315680627787ace
lotkrt <juan.incacutipa@ucsp.edu.pe>
2025-05-28 11:28:12 -0500

    start project

---
+++ src/empleado.cpp
@@ -0,0 +1,20 @@
+#include "../include/empleado.hpp"
+#include <string>
+
+
+ 
+
+empleado::empleado():name(""), {};
+  
+  void empleado::setName();
+  void empleado::setLastName();
+  void empleado::setEmail();
+  void empleado::setID();
+  
+  std::string empleado::getName();
+  std::string empleado::getLastName();
+  std::string empleado::getEmail();
+
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../include/empleado.hpp"
+
+int main () {
+  empleado empleado1;
+
+  
+
+
+
+  return 0;
+}
